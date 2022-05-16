d649e57f4bcb80766faa12001188b2fdbe4fe3b1
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-05-16 16:26:17 +0300

    Add JavaFX GUI

---
+++ LaboratoryWork8/lab-client/src/main/java/kib/lab8/client/gui/GUIApplication.java
@@ -0,0 +1,16 @@
+package kib.lab8.client.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUIApplication extends Application {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
