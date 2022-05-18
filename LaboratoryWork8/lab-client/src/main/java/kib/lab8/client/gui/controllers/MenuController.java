8ba86cab427fd8dee806a686d5c2544fc9d72543
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-05-18 07:52:25 +0300

    start realizing of main window

---
+++ LaboratoryWork8/lab-client/src/main/java/kib/lab8/client/gui/controllers/MenuController.java
@@ -0,0 +1,15 @@
+package kib.lab8.client.gui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MenuController {
+    @FXML
+    private Button button;
+
+    @FXML
+    private void buttonClicked() {
+        System.out.println("dada");
+        button.setText("abhsba");
+    }
+}
