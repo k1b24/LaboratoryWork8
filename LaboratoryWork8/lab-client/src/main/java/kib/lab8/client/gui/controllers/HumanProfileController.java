80c0920469d34e1c8a0347c02a448e3391c762df
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-05-30 19:22:20 +0300

    add human profile

---
+++ LaboratoryWork8/lab-client/src/main/java/kib/lab8/client/gui/controllers/HumanProfileController.java
@@ -0,0 +1,77 @@
+package kib.lab8.client.gui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+
+public class HumanProfileController {
+
+    @FXML
+    private Text nameValue;
+
+    @FXML
+    private Text creationDateValue;
+
+    @FXML
+    private Text xValue;
+
+    @FXML
+    private Text yValue;
+
+    @FXML
+    private Text realHeroValue;
+
+    @FXML
+    private Text popularityValue;
+
+    @FXML
+    private Text impactSpeedValue;
+
+    @FXML
+    private Text weaponValue;
+
+    @FXML
+    private Text moodValue;
+
+    @FXML
+    private Text carValue;
+
+    @FXML
+    private Text carCoolnessValue;
+
+    @FXML
+    private Text carSpeedValue;
+
+    @FXML
+    private Text authorValue;
+
+    @FXML
+    private Button updateButton;
+
+    @FXML
+    private Button removeButton;
+
+    @FXML
+    private Button closeButton;
+
+    @FXML
+    private void initialize() {
+        //TODO енэйблить и дисэйблить кнопки в зависимости от того, кто открыл
+    }
+
+    @FXML
+    private void update() {
+
+    }
+
+    @FXML
+    private void remove() {
+
+    }
+
+    @FXML
+    private void close() {
+
+    }
+}
--- LaboratoryWork8/lab-client/src/main/java/kib/lab8/client/gui/controllers/UserProfileController.java
+++
@@ -1,4 +0,0 @@
-package kib.lab8.client.gui.controllers;
-
-public class UserProfileController {
-}
