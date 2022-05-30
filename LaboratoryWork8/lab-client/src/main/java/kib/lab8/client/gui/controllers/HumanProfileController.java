f9c3fdd0b77057f2ca46d4f758ec7061bfbd0d2b
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-05-30 20:36:25 +0300

    add updating from human profile

--- LaboratoryWork8/lab-client/src/main/java/kib/lab8/client/gui/controllers/HumanProfileController.java
+++ LaboratoryWork8/lab-client/src/main/java/kib/lab8/client/gui/controllers/HumanProfileController.java
@@ -1,8 +1,16 @@
 package kib.lab8.client.gui.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import kib.lab8.common.entities.enums.Mood;
+import kib.lab8.common.entities.enums.WeaponType;
 
 
 public class HumanProfileController {
@@ -56,8 +64,60 @@ public class HumanProfileController {
     private Button closeButton;
 
     @FXML
+    private VBox updateVBox;
+
+    @FXML
+    private VBox valuesVBox;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private Text creationDateUpdate;
+
+    @FXML
+    private TextField xField;
+
+    @FXML
+    private TextField yField;
+
+    @FXML
+    private CheckBox realHeroCheckBox;
+
+    @FXML
+    private CheckBox popularityCheckBox;
+
+    @FXML
+    private TextField impactSpeedField;
+
+    @FXML
+    private ChoiceBox<WeaponType> weaponChoiceBox;
+
+    @FXML
+    private ChoiceBox<Mood> moodChoiceBox;
+
+    @FXML
+    private CheckBox carCheckBox;
+
+    @FXML
+    private CheckBox carCoolnessCheckBox;
+
+    @FXML
+    private TextField carSpeedField;
+
+    @FXML
+    private Text authorUpdate;
+
+    private final ObservableList<WeaponType> weaponTypes = FXCollections.observableArrayList(WeaponType.values());
+    private final ObservableList<Mood> moods = FXCollections.observableArrayList(Mood.values());
+
+    @FXML
     private void initialize() {
         //TODO енэйблить и дисэйблить кнопки в зависимости от того, кто открыл
+        weaponChoiceBox.setItems(weaponTypes);
+        moodChoiceBox.setItems(moods);
+        updateVBox.setVisible(false);
+        valuesVBox.setVisible(true);
     }
 
     @FXML
