7fe9821b0bc73afbd0d1ce06c3de68433c6be979
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-05-30 18:34:48 +0300

    logic

--- LaboratoryWork8/lab-client/src/main/java/kib/lab8/client/gui/controllers/VisualizationController.java
+++ LaboratoryWork8/lab-client/src/main/java/kib/lab8/client/gui/controllers/VisualizationController.java
@@ -1,5 +1,8 @@
 package kib.lab8.client.gui.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import kib.lab8.client.gui.abstractions.DataVisualizerController;
 import kib.lab8.common.entities.HumanBeing;
 
@@ -7,7 +10,16 @@ import java.util.List;
 
 public class VisualizationController extends DataVisualizerController {
 
+    @FXML
+    private Canvas canvas;
+    private GraphicsContext graphics;
 
+    @FXML
+    private void initialize() {
+        graphics = canvas.getGraphicsContext2D();
+        graphics.strokeLine(10,10,20,20);
+
+    }
 
     @Override
     public void updateInfo(List<HumanBeing> humanBeingList) {
