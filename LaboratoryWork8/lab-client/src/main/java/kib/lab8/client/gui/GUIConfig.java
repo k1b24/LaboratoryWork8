ba61c608a34c4ef494f3c3b5c7bccf1ee8d9145b
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-06-04 14:59:47 +0300

    fix file-naming

--- LaboratoryWork8/lab-client/src/main/java/kib/lab8/client/gui/GUIConfig.java
+++ LaboratoryWork8/lab-client/src/main/java/kib/lab8/client/gui/GUIConfig.java
@@ -8,17 +8,17 @@ import javafx.scene.image.Image;
 public class GUIConfig {
     private static final String CORNER_IMAGE = "/icons/img.png";
     public static final String TITLE = "BE HUMAN";
-    public static final String CONNECTION_PATH = "/layout/serverConnection.fxml";
+    public static final String CONNECTION_PATH = "/layout/server_connection.fxml";
     public static final String REGISTRATION_PATH = "/layout/registration.fxml";
     public static final String AUTHORIZATION_PATH = "/layout/authorization.fxml";
-    public static final String MAIN_WINDOW_PATH = "/layout/mainApp.fxml";
+    public static final String MAIN_WINDOW_PATH = "/layout/menu.fxml";
     public static final String TABLEVIEW_PATH = "/layout/tableview.fxml";
-    public static final String SETTINGS_PANE_PATH = "/layout/settingsPane.fxml";
-    public static final String ADD_MENU_PATH = "/layout/addCommand.fxml";
+    public static final String SETTINGS_PANE_PATH = "/layout/settings_pane.fxml";
+    public static final String ADD_MENU_PATH = "/layout/add_command.fxml";
     public static final String VISUAL_PANE_PATH = "/layout/visualization.fxml";
-    public static final String REMOVE_WINDOW_PATH = "/layout/removeWindow.fxml";
-    public static final String UPDATE_WINDOW_PATH = "/layout/updateByIdWindow.fxml";
-    public static final String PROFILE_WINDOW_PATH = "/layout/humanProfile.fxml";
+    public static final String REMOVE_WINDOW_PATH = "/layout/remove_window.fxml";
+    public static final String UPDATE_WINDOW_PATH = "/layout/update_by_id_window.fxml";
+    public static final String PROFILE_WINDOW_PATH = "/layout/human_profile.fxml";
 
 
     public static Image getCornerImage() {
